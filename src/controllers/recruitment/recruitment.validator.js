ad735af82f320a9c4208bf2d348ec8172a4a5704
chldppwls12 <chldppwls12@naver.com>
2022-06-22 10:09:54 +0900

    🎨 채용공고 등록 시 길이 제한 validation 추가

--- src/controllers/recruitment/recruitment.validator.js
+++ src/controllers/recruitment/recruitment.validator.js
@@ -3,9 +3,9 @@ import Joi from 'joi';
 export const addRecruitment = {
   body: Joi.object({
     company_id: Joi.number().required(),
-    position: Joi.string().required(),
-    compensation: Joi.number().required(),
-    content: Joi.string().required(),
-    skill: Joi.string().required(),
+    position: Joi.string().max(30).required(),
+    compensation: Joi.number().max(20).required(),
+    content: Joi.string().max(2000).required(),
+    skill: Joi.string().max(20).required(),
   })
 };
